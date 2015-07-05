3bc349ad1bf63a940cdd8269bbaf3acbb1fceb42
HuCheng <huchengcn@126.com>
2015-07-05 11:58:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+/*总算是找到解决响应式访问网站的方法了！*/
 /*判断访问设备定义全局变量*/
     $userAgent = $_SERVER['HTTP_USER_AGENT'];
 	if(strpos($userAgent,"iPhone") || strpos($userAgent,"iPad") || strpos($userAgent,"iPod") || strpos($userAgent,"Android")){
